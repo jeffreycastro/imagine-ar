8cea98ab95b898c2872905e28e2b40e112e6d0c7
Jeffrey Castro <castrojeffreym@gmail.com>
2017-05-21 21:23:39 +0800

    Update for imaginear instead of imagesblues

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,39 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ImagesBlues
+module ImagineAR
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
+    # Mailer Settings (global)
+    config.action_mailer.default_url_options = { :host => "localhost:3000" }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp #:sendmail #:smtp
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'info.imaginear@gmail.com',
+      :password             => '1Maginear2017!',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+    }
+
+    # config.action_mailer.default_url_options = { :host => "localhost:3000" }
+    # config.action_mailer.perform_deliveries = true
+    # config.action_mailer.delivery_method = :smtp #:sendmail #:smtp
+    # config.action_mailer.raise_delivery_errors = true
+    # config.action_mailer.smtp_settings = {
+    #   :address              => "mail.imagine-ar.com",
+    #   :port                 => 465,
+    #   :user_name            => 'info@imagine-ar.com',
+    #   :password             => '1Maginear2017!',
+    #   :authentication       => 'plain',
+    #   :enable_starttls_auto => true
+    # }
   end
 end
